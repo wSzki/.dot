cc2bfe17da8837781e6d00f28781f43c3b5d6655
wszki <wsz@anche.no>
2020-09-13 13:57:21 +0200

    hi

---
+++ sys_config/setup.sh
@@ -0,0 +1,5 @@
+# Acpi = Battery monitor
+sudo pacman -S --noconfirm acpi
+sudo cp ./99-lowbat.rules /etc/udev/rules.d/
+sudo cp ./40-touchpad.conf /etc/X11/xorg.conf.d/
+sudo cp ./loader.conf /boot/loader
