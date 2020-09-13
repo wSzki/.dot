fc3f56630bfdaf6cb1e7948b2c38c510400920ca
wszki <wsz@anche.no>
2020-09-13 14:08:11 +0200

    hi

--- sys_config/setup.sh
+++ sys_config/setup.sh
@@ -3,3 +3,4 @@ sudo pacman -S --noconfirm acpi
 sudo cp ./99-lowbat.rules /etc/udev/rules.d/
 sudo cp ./40-touchpad.conf /etc/X11/xorg.conf.d/
 sudo cp ./loader.conf /boot/loader
+sudo cp ./logind.conf /etc/systemd/
